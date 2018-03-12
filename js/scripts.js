af96613a47f5ae98bfbf35b107949e1ba6318599
Joanna Fengler <asiapobuta@wp.pl>
2018-03-12 20:00:42 +0100

    add comments in js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Hello.
+//
+// This is a page that makes you happy and detect
+// problems in your JavaScript code.
+
+var name = prompt('Enter your name');
+/*alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
